dc3a346b2e06f71c3b699e6dadf212a461c129b7
esimms999 <esimms@yahoo.com>
2022-12-05 03:16:48 +0000

    Day 3, Part 1.

--- R/day03.R
+++ R/day03.R
@@ -4,28 +4,33 @@
 library(tidyverse)
 
 # Read in example or full data
- data_day03_01 <- read_table("Data/data_day03_01_example", col_names = "fullString")
-#data_day02_01 <- read_table("Data/data_day02_01_full",
-#  col_names = c("elf_choice", "my_choice"),
-#  skip_empty_rows = FALSE
-#)
+# data_day03_01 <- read_table("Data/data_day03_01_example", col_names = "fullString")
+data_day03_01 <- read_table("Data/data_day03_01_full", col_names = "fullString")
 
 # ----- Part1 -----
 #
+lowerUpper <- str_c(
+  paste(letters, collapse = ""),
+  paste(LETTERS, collapse = "")
+)
+
 df <- data_day03_01 %>%
-   mutate(fullStringLength=str_length(fullString),
-          halfStringLength=fullStringLength/2,
-          firstHalf=substr(fullString,1,halfStringLength),
-          secondHalf=substr(fullString, halfStringLength+1,fullStringLength))
+  mutate(
+    fullStringLength = str_length(fullString),
+    halfStringLength = fullStringLength / 2,
+    firstHalf = substr(fullString, 1, halfStringLength),
+    secondHalf = substr(fullString, halfStringLength + 1, fullStringLength)
+  ) %>%
+  mutate(dupLetter=str_extract(firstHalf,
+                               paste0("[", secondHalf, "]")))
 
-df2 <-df %>%
-  mutate(dupLetter=str_extract(firstHalf,paste0("[",secondHalf,"]")))%>%
-  mutate(score=str_locate(lowerUpper,dupLetter)[1])
-
-
-lowerUpper <- str_c(paste(letters,collapse=''),
-paste(LETTERS, collapse='') )
-lowerUpper
+# Terrible time trying to find letter's position ... due to
+# vectorization(?). Forced with this mess, but there must
+# be a better way!
+dupLetter_vector<-df$dupLetter
+dupLetter_position<-str_locate(lowerUpper,dupLetter_vector)
+total_score<-sum(dupLetter_position[,1])
+cat("Part 1: Total score: ", total_score, "\n")
 
 # ----- Part 2 -----
 #
@@ -33,4 +38,3 @@ lowerUpper
 # Structure data:
 
 # ----- Print results -----
-
