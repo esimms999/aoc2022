b21860664921425923e27d13a261c51ec74f64f1
esimms999 <esimms@yahoo.com>
2022-12-12 21:23:25 +0000

    Day 4 - final.

--- R/day04.R
+++ R/day04.R
@@ -1,53 +1,36 @@
-# AoC2022: Day05
-# The answers are x and y.
+# AoC2022: Day04
+# The answers are 459 and 779.
 
 library(tidyverse)
+
+# Read in example or full data
+# Note: modified to replace hyphens with commas!!
+# data_day04_01 <- read_csv("Data/data_day04_01_example2", col_names = c("x1","x2","y1","y2"))
+data_day04_01 <- read_csv("Data/data_day04_01_full2", col_names = c("x1","x2","y1","y2"))
 
 # ----- Part1 -----
 #
 
-# Set up example or full data
-# Example:
-stack1<-"ZN"
-stack2<-"MCD"
-stack3<-"P"
+df <- data_day04_01 %>%
+  mutate(enclosed=ifelse((x1 == y1 | x2==y2 | x1==y2|x2==y1) |
+                         (x1 < y1 & x2 >= y1) | (y1 < x1 & y2 >= x1), 1, 0)) %>%
+  summarise(total_enclosed = sum(enclosed, na.rm = TRUE))
 
-move_blocks_to <- function(amount, from_stack, to_stack) {
-  for (i in 1:amount) {
-    str_sub(to_stack,str_length(to_stack)+1) <-
-      str_sub(from_stack,str_length(from_stack))
-    #cat("to_stack: ", to_stack, "   from_stack: ", from_stack, "\n")
-  }
-  return(to_stack)
-}
+answer<-df %>%
+  pull(total_enclosed)
 
-move_blocks_from <- function(amount, from_stack, to_stack) {
-  for (i in 1:amount) {
-    str_sub(from_stack,str_length(from_stack)) <- ""
-    #cat("to_stack: ", to_stack, "   from_stack: ", from_stack, "\n")
-  }
-  return(from_stack)
-}
+answer
 
-cat("Stack1: ", stack1, "   Stack2: ", stack2, "   Stack3: ", stack3, "\n")
-
-stack1 <- move_blocks_to(1,stack2,stack1)
-stack2 <- move_blocks_from(1,stack2,stack1)
-cat("Stack1: ", stack1, "   Stack2: ", stack2, "   Stack3: ", stack3, "\n")
-
-stack3 <- move_blocks_to(3,stack1,stack3)
-stack1 <- move_blocks_from(3,stack1,stack3)
-cat("Stack1: ", stack1, "   Stack2: ", stack2, "   Stack3: ", stack3, "\n")
-
-stack1 <- move_blocks_to(2,stack2,stack1)
-stack2 <- move_blocks_from(2,stack2,stack1)
-cat("Stack1: ", stack1, "   Stack2: ", stack2, "   Stack3: ", stack3, "\n")
-
-stack2 <- move_blocks_to(1,stack1,stack2)
-stack1 <- move_blocks_from(1,stack1,stack2)
-cat("Stack1: ", stack1, "   Stack2: ", stack2, "   Stack3: ", stack3, "\n")
 
 # ----- Part 2 -----
 #
 
+df <- data_day04_01 %>%
+  mutate(enclosed=ifelse((x1 ) | (x1 <= y1 & x2 >= y2), 1, 0)) %>%
+  summarise(total_enclosed = sum(enclosed, na.rm = TRUE))
+
+answer<-df %>%
+  pull(total_enclosed)
+
+answer
 # ----- Print results -----
